c4d08b885d7a865942a17597ade06e997a1855e4
= <=>
2022-03-26 20:09:06 +0700

    yeay

---
+++ kuis/web-reactku/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+<head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link></head>
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p>copyright @2020</p>
+            <div className="social">
+                <i className="fa fa-facebook"></i>
+                <i className="fa fa-instagram"></i>
+            </div>
+            
+        </div>
+    )
+}
---
+++ kuis/web-reactku/src/components/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import "components/FontAwesomeIcon";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+import { faSearch, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+
+
+
+export default function Header(){
+    return(
+        <nav>
+            <div className="logo">Rayes.</div>
+            <ul>
+                <li>Home</li>
+                <li>Our Products</li>
+                <li>About Us</li>
+                <li>Contact</li>
+            </ul>
+            <div className="search">
+                <FontAwesomeIcon icon = {faSearch} />
+                <FontAwesomeIcon icon = {faShoppingBasket} />
+            </div>
+        </nav>
+    )
+}
